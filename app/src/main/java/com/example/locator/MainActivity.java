cc90ec50bb1af0521c8419bbf79ffef542cb159b
Bola Bally <bolabally@gmail.com>
2020-08-24 14:02:13 +0000

    added permission

--- app/src/main/java/com/example/locator/MainActivity.java
+++ app/src/main/java/com/example/locator/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.locator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // to see if there's tracking
+    LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+       if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        finish();
+    }
+    //see if the app has access to the location permission//
+    int permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION);
+
+    //If the location permission has been granted, start tracking
+
+       if (permission == PackageManager.PERMISSION_GRANTED) {
+        startTrackerService();
+    } else {
+
+        //Request access if the app doesn't currently have access to the user's location//
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
+            grantResults) {
+
+        //If the permission has been granted, start tracking//
+
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            startTrackerService();
+        } else {
+
+//If the user denies the permission request, display a toast with more information//
+
+            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //Start the TrackerService//
+
+    private void startTrackerService() {
+        startService(new Intent(this, TrackingService.class));
+
+//Notify the user that tracking has been enabled//
+
+        Toast.makeText(this, "GPS tracking enabled", Toast.LENGTH_SHORT).show();
+
+//Close MainActivity//
+
+        finish();
+    }
+
 }
+
+
