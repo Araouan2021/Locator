1473374ce83ab16038b45cdd6d0f676fbf4415bb
Bola Bally <bolabally@gmail.com>
2020-08-24 22:33:44 +0000

    addMainActivity.java

--- app/src/main/java/com/example/locator/MainActivity.java
+++ app/src/main/java/com/example/locator/MainActivity.java
@@ -1,75 +1,75 @@
 package com.example.locator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.test.mock.MockPackageManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button btnShowLocation;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    // GPSTracker class
+    GPSTracker gps;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    // to see if there's tracking
-    LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-       if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-        finish();
-    }
-    //see if the app has access to the location permission//
-    int permission = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.ACCESS_FINE_LOCATION);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != MockPackageManager.PERMISSION_GRANTED) {
 
-    //If the location permission has been granted, start tracking
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
 
-       if (permission == PackageManager.PERMISSION_GRANTED) {
-        startTrackerService();
-    } else {
-
-        //Request access if the app doesn't currently have access to the user's location//
-
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSIONS_REQUEST);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
-            grantResults) {
-
-        //If the permission has been granted, start tracking//
-
-        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            startTrackerService();
-        } else {
-
-//If the user denies the permission request, display a toast with more information//
-
-            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+                // If a permission is not allowed by the user, this condition will
+                execute every time, else your else part will work
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-    //Start the TrackerService//
 
-    private void startTrackerService() {
-        startService(new Intent(this, TrackingService.class));
+        btnShowLocation = (Button) findViewById(R.id.button);
 
-//Notify the user that tracking has been enabled//
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
-        Toast.makeText(this, "GPS tracking enabled", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
 
-//Close MainActivity//
+                // check if GPS enabled
+                if(gps.canGetLocation()){
 
-        finish();
-    }
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
 
-}
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
+                            + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+
+
+
+
+
+
 
 
